e08cb082b9a49e315c7b86ee027acb68ece552d2
Parkgeunhyung <rootbro@kakao.com>
2020-02-20 00:15:04 +0900

    first commit

---
+++ ex_idus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ex_idus
+//
+//  Created by 박근형 on 2020/02/20.
+//  Copyright © 2020 pgh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
